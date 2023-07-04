ad918107f60cde1d5dfca716cf8b49b276594eb8
KerriRentmeester <130302053+KerriRentmeester@users.noreply.github.com>
2023-07-04 13:39:09 -0500

    edits were made. Bus API was removed.

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+	google_maps_api_key : 'AIzaSyAQwqgTnGv2Lw1-7zYnUyapu5J1t_Y5VYU',  //Google maps
+	mbta_bus_api_key: 'ca34f7b7ac8a445287cab52fb451030a',  // Massachusetts Bay Area
+	mta_api_key: 'f5b6494d-90b5-4f6a-b9dc-f4fe08b04248',  // NYC
+	cta_bus_api_key: '3UtwrnF5eBuyNsQfKcx3n3Xi2' // Chicago
+}
